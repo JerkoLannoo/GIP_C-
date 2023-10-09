6bf016233d13bfed9642b447309c91af33bb151f
Jerko Lannoo <Jerko Lannoo@DESKTOP-UES769J>
2023-10-09 19:59:56 +0200

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.info = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loading_icon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.loading_icon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // info
+            // 
+            this.info.Dock = System.Windows.Forms.DockStyle.Top;
+            this.info.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.info.Location = new System.Drawing.Point(0, 0);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(800, 156);
+            this.info.TabIndex = 0;
+            this.info.Text = "Scan je leerlingenkaart.";
+            this.info.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.info.Click += new System.EventHandler(this.info_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(582, 337);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // loading_icon
+            // 
+            this.loading_icon.BackColor = System.Drawing.Color.Transparent;
+            this.loading_icon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loading_icon.Image = ((System.Drawing.Image)(resources.GetObject("loading_icon.Image")));
+            this.loading_icon.Location = new System.Drawing.Point(0, 156);
+            this.loading_icon.Name = "loading_icon";
+            this.loading_icon.Size = new System.Drawing.Size(800, 324);
+            this.loading_icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.loading_icon.TabIndex = 4;
+            this.loading_icon.TabStop = false;
+            this.loading_icon.Click += new System.EventHandler(this.loading_icon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.loading_icon);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.info);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.focusOpTextbox);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.checkEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.loading_icon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label info;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox loading_icon;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+
+
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        string code = "";
+        bool login = false;
+        int status = 0;
+    
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Hide();
+            loading_icon.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+
+  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;
+        }
+
+        private void focusOpTextbox(object sender, EventArgs e)
+        {
+            this.ActiveControl = textBox1;
+        }
+
+    
+
+  
+
+        private async void checkEnter(object sender, KeyPressEventArgs e)
+        {
+            info.Text = code;
+            if (e.KeyChar == (char)Keys.Escape) this.Close();
+          else  if (e.KeyChar == (char)13)
+            {
+                if (code.Length != 5)
+                {
+                    info.Text = "Ongeldige Barcode.";
+                    code = "";
+                    this.ActiveControl = textBox1;
+                    Thread workerThread = new Thread(new ThreadStart(Print));
+                    workerThread.Start();
+
+                }
+                else
+                {
+                    status = 0;
+                    info.Text = "Barcode controleren...";
+                    loading_icon.Visible = true;
+                    Thread workerThread = new Thread(new ThreadStart(Print));
+                    // workerThread.Start();
+               
+                   await SendInfo();
+                   
+                    Form2 form2 = new Form2();
+                    form2.code = code;
+                   
+                    if (login&&status==1)
+                    {
+                        info.Text = "Scan je leerlingenkaart.";
+                      
+                     
+                        form2.ShowDialog();
+                    }
+                    else if (status == 1 && !login)
+                    {
+                    
+                        info.Text = "Ongeldige leerlingenkaart.";
+                        Thread.Sleep(3000);
+                        info.Invoke((MethodInvoker)(() => info.Text = "Scan je leerlingenkaart."));
+
+                    }
+                    else if(status==2)
+                    {
+
+                      
+                        info.Text = "Scan je leerlingenkaart.";
+
+                    }
+                    else
+                        //  form2.ShowDialog();
+                        code = "";
+                    
+                    this.ActiveControl = textBox1;
+                }
+
+            }
+            else
+            {
+                code += e.KeyChar;
+            }
+            
+
+        }
+
+        private async Task  SendInfo()
+        {
+            try
+            {
+                var values ="{\"code\":\"" + code + "\"}";
+                JObject json = JObject.Parse(values);
+
+                var jsonString = JsonConvert.SerializeObject(json);
+                
+                
+                var content = new StringContent(values, Encoding.UTF8, "application/json");
+
+
+                var response = await client.PostAsync("http://192.168.100.3/", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    JObject jsonObject = JObject.Parse(responseString);
+                    string value = jsonObject["login"].ToString();
+                    login = Convert.ToBoolean(value);
+                    loading_icon.Visible = false;
+                    status = 1;
+                    code = "";
+                }
+                else
+                {
+                    info.Text = "Er ging iets mis.";
+                    Thread.Sleep(3000);
+                    code = "";
+                    loading_icon.Visible = false;
+                    status = 2;
+                }
+            }
+            catch (Exception ex)
+            {
+                code = "";
+                status = 2;
+                info.Text = "Er ging iets mis. (server is onbereikbaar)";
+                loading_icon.Visible = false;
+                Thread.Sleep(3000);
+            }
+    
+            
+        }
+     
+        private  void Print()
+        { 
+            string oudInfo = info.Text;
+            while (true)
+            {
+
+                
+                if (info.Text == "Barcode controleren.")
+                {
+                    while (status == 0)
+                    {
+
+                        if (info.Text == "Barcode controleren...") info.Invoke((MethodInvoker)(() => info.Text = "Barcode controleren."));
+                        else if (info.Text == "Barcode controleren..") info.Invoke((MethodInvoker)(() => info.Text = "Barcode controleren..."));
+                        else if (info.Text == "Barcode controleren.") info.Invoke((MethodInvoker)(() => info.Text = "Barcode controleren.."));
+
+                        Thread.Sleep(500);
+
+                    }
+
+
+                }
+
+                if (info.Text == "Ongeldige Barcode." || info.Text == "Ongeldige leerlingenkaart." || info.Text == "Er ging iets mis." || info.Text == "Er ging iets mis. (server is onbereikbaar)") {
+                    Thread.Sleep(3000);
+                    info.Invoke((MethodInvoker)(() => info.Text = "Scan je leerlingenkaart."));
+                } 
+            }
+     
+         
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loading_icon_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
