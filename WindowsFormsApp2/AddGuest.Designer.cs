95cf09b247abf43fa38e839d41d63119e3ee4c13
LannooJerko <jerko.lannoo@sgsintpaulus.eu>
2023-11-17 08:49:06 +0100

    spelfouten

--- WindowsFormsApp2/AddGuest.Designer.cs
+++ WindowsFormsApp2/AddGuest.Designer.cs
@@ -158,9 +158,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(12, 142);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.Size = new System.Drawing.Size(74, 20);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Duratie: ";
+            this.label3.Text = "Duratief: ";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
--- WindowsFormsApp2/addBeurt.Designer.cs
+++ WindowsFormsApp2/addBeurt.Designer.cs
@@ -81,9 +81,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(12, 144);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.Size = new System.Drawing.Size(74, 20);
             this.label3.TabIndex = 20;
-            this.label3.Text = "Duratie: ";
+            this.label3.Text = "Duratief: ";
             // 
             // duration
             // 
